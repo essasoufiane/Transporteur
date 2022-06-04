45d1d070dbd676b41dabcd27ff9a9cd9d42c5e36
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-06-04 16:05:46 +0200

    + form d'inscription avec commande php bin/console make:registration-form

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                "label" => "Prenom",
+                "attr" => ["placeholder" => "Votre prénom"]
+            ])
+            ->add('lastname', TextType::class, [
+                "label" => "Nom de famille",
+                "attr" => ["placeholder" => "Votre nom de famille"]
+            ])
+            ->add('adresse', TextType::class, [
+                "label" => "Votre adresse",
+                "attr" => ["placeholder" => "25 rue de la farfouille, Paris"]
+            ])
+            ->add('email', EmailType::class, [
+                "label" => "Email",
+                "attr"  => ["placeholder" => "Votre email"]
+            ])
+            ->add('age', IntegerType::class, [
+                "label" => "Age",
+                "attr"  => ["placeholder" => "25"]
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
