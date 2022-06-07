102cbef660013aa444edcfaf51bf1f7503560161
camara92 <88945535+camara92@users.noreply.github.com>
2022-06-07 12:01:45 +0200

    MAJ

--- src/Form/OrderType.php
+++ src/Form/OrderType.php
@@ -5,7 +5,6 @@ namespace App\Form;
 use App\Entity\Order;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,14 +17,8 @@ class OrderType extends AbstractType
                 "label" => "Saisir une adresse de départ",
                 "attr"  => ["placeholder" => "Départ Paris"]
             ])
-            ->add('destination', TextType::class, [
-                "label" => "Saisir une adresse de destination",
-                "attr"  => ["placeholder" => "25 Rue de la République à Marseille"]
-            ])
-            ->add('date_reservation', DateType::class, [
-                "label" => "Saisir une date de réservation",
-                "attr"  => ["placeholder" => "01 Janvier 2022 à 04h00"]
-            ])
+            ->add('destination')
+            ->add('date_reservation')
             //->add('date_creation')
             // ->add('user_id')
         ;
