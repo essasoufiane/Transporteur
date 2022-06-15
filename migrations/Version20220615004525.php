ef1090120a1eca29cd398aa2264d59e1cc971040
camara92 <88945535+camara92@users.noreply.github.com>
2022-06-15 03:06:48 +0200

    +Ajout de la propriété image dans entité user
    Afin d'afficher l'image de profile dans la vue user si inscrit bien-sûr.

--- migrations/Version20220615004525.php
+++ migrations/Version20220615004525.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220607132102 extends AbstractMigration
+final class Version20220615004525 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,12 @@ final class Version20220607132102 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE contact (id INT AUTO_INCREMENT NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, message LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL');
+        $this->addSql('ALTER TABLE user ADD image VARCHAR(255) DEFAULT NULL, CHANGE roles roles JSON NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE contact');
-        $this->addSql('ALTER TABLE user CHANGE roles roles JSON DEFAULT NULL');
+        $this->addSql('ALTER TABLE user DROP image, CHANGE roles roles JSON DEFAULT NULL');
     }
 }
