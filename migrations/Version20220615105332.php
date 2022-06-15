131874b104e85b5607947a51fb244f5454f784d8
Hajara Ledlum <ledlumh@gmail.com>
2022-06-15 13:16:14 +0200

    +card accueil h-100

--- migrations/Version20220615105332.php
+++ migrations/Version20220615105332.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220615004525 extends AbstractMigration
+final class Version20220615105332 extends AbstractMigration
 {
     public function getDescription(): string
     {
