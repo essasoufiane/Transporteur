ae53c6c71732ec88a690a477a6a86d08c68ef776
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-09-20 01:44:24 +0200

    correction mineure

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ContactRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ContactRepository::class)]
 class Contact
